a4feae3612a79abc33ae3a29a2647b60944def4b
John-Hole <giovannibuco2020@gmail.com>
2026-05-15 23:20:36 +0200

    Fix PWA for Android/iOS, add logo, remove Premium from name

--- sw.js
+++ sw.js
@@ -1,56 +1,22 @@
-const CACHE_NAME = 'streamgrid-v2';
-const urlsToCache = [
+const CACHE_NAME = 'stream-grid-v1';
+const ASSETS = [
   './',
   './index.html',
-  './index.css',
   './player.html',
   './tutorial.html',
-  './stream-in-colorado.jpg',
-  './manifest.json'
+  './index.css',
+  './manifest.json',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request).then(
-          function(response) {
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            var responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-            return response;
-          }
-        );
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
