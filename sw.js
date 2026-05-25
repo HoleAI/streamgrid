d9f77b04d183ed034a7105e824a33d465c98edf2
John-Hole <giovannibuco2020@gmail.com>
2026-05-25 16:25:21 +0200

    Fix PWA cache: upgrade to v2 and use network-first strategy

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'stream-grid-v1';
+const CACHE_NAME = 'stream-grid-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -6,17 +6,32 @@ const ASSETS = [
   './tutorial.html',
   './index.css',
   './manifest.json',
-  './icon-512.png'
+  './stream-in-colorado.jpg'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
